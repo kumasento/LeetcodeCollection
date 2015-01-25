4138f32c0743f6ad003670da008348cc711a0fde
Vincent Zhao <vincentzhaorz@gmail.com>
2015-01-25 16:27:15 +0800

    sync

---
+++ hard/SearchinRotatedSortedArray.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int search(int A[], int n, int target)
+{
+    
+}
+
+int main() 
+{
+    int A[] = { 4, 5, 6, 7, 0, 1, 2 };
+
+    cout << search(A, 7, 6) << endl;
+
+    return 0;
+}
